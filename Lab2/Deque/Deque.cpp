a9eb67ac25cb6948a07c05e897d8cdaee56a4450
Ilya Yakovlev <ilyayakovlev0123@gmail.com>
2022-03-28 22:30:56 +0300

    Deque class finished

---
+++ Lab2/Deque/Deque.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Deque.h"
+using namespace std;
+
+int main()
+{
+	Deque d;
+	/*d.push_front(1);
+	d.push_front(2);
+	d.push_front(3);
+	d.push_front(4);*/
+	d.push_back(1);
+	d.push_back(2);
+	d.push_back(3);
+	d.push_back(4);
+
+	d.show();
+}
---
+++ Lab2/Deque/Deque.h
@@ -0,0 +1,118 @@
+#pragma once
+using namespace std;
+
+struct Node {
+	Node* Next, * Prev;
+	int data;
+};
+
+class Deque {
+private:
+	
+public:
+	Node* Head, * Tail;
+	int count;
+	Deque() {
+		Head = Tail = nullptr;
+		count = 0;
+	}
+	void push_back(int x) {
+		Node* temp = new Node;
+		temp->data = x;
+		if (Head)
+		{
+			temp->Prev = Tail;
+			temp->Next = nullptr;
+			Tail->Next = temp;
+			Tail = temp;
+		}
+		else
+		{
+			Head = Tail = temp;
+		}
+		count++;
+		temp = nullptr;
+		delete temp;
+	}
+	void push_front(int x) {
+		Node* temp = new Node;
+		temp->data = x;
+		if (Head)
+		{
+			temp->Next = Head;
+			Head->Prev = temp;
+			temp->Prev = nullptr;
+			Head = temp;
+		}
+		else
+		{
+			Head = Tail = temp;
+		}
+		count++;
+		temp = nullptr;
+		delete temp;
+	}
+	void pop_front() {
+		Node* temp = Head;
+		Head = Head->Next;
+		temp = nullptr;
+		delete temp;
+		count--;
+	}
+	void pop_back() {
+		Node* temp = Tail;
+		Tail = Tail->Prev;
+		temp = nullptr;
+		delete temp;
+		count--;
+	}
+	void popAll() {
+		while (Head)
+		{
+			pop_front();
+		}
+	}
+	int getFront() {
+		return Head->data;
+	}
+	int getBack() {
+		return Tail->data;
+	}
+	int get(int x) {
+		if (x < count)
+		{
+			Node* temp = Head;
+			int i = 0;
+			while (temp != Tail->Next)
+			{
+				if (x == i)
+				{
+					return temp->data;
+				}
+				i++;
+				temp = temp->Next;
+			}
+		}
+	}
+	bool isEmpty() {
+		if (Head)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	void show() {
+		Node* temp = Head;
+		while (temp != Tail->Next)
+		{
+			cout << temp->data << endl;
+			temp = temp->Next;
+		}
+		cout << "Amount: " << count << endl;
+		temp = nullptr;
+		delete temp;
+	}
+};
