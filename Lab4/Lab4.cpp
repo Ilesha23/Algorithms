faeaee1109cabffa47ba5414cd783e58d7f2e542
Ilya Yakovlev <ilyayakovlev0123@gmail.com>
2022-04-21 21:01:18 +0300

    Lab4

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -1,9 +1,91 @@
 ﻿#define N 10000
 
 #include <iostream>
+#include <iomanip>
 #include <time.h>
 
 using namespace std;
+
+void FillRand(int* arr);
+void FillUp(int* arr);
+void FillDown(int* arr);
+void show(int* arr);
+void swap(int& a, int& b);
+void InsertionSort(int* arr);
+void ShellSort(int* arr);
+void SelectionSort(int* arr);
+int increment(long  inc[], long  size);
+void ShellInc(int* arr);
+
+int main()
+{
+	int* arr = new int[N];
+
+	cout.setf(ios::fixed);
+	cout.precision(6);
+
+	FillRand(arr);
+	clock_t start1 = clock();
+	InsertionSort(arr);
+	clock_t end1 = clock();
+	cout << setw(25) << "Insertion sort (random): " << setw(10) << (double)(end1 - start1) / CLOCKS_PER_SEC << endl;
+
+	FillUp(arr);
+	start1 = clock();
+	InsertionSort(arr);
+	end1 = clock();
+ 	cout << setw(25) << "Insertion sort (Up): " << setw(10) << (double)(end1 - start1) / CLOCKS_PER_SEC << endl;
+
+	FillDown(arr);
+	start1 = clock();
+	InsertionSort(arr);
+	end1 = clock();
+	cout << setw(25) << "Insertion sort (Down): " << setw(10) << (double)(end1 - start1) / CLOCKS_PER_SEC << endl << endl;
+
+
+
+	FillRand(arr);
+	clock_t start2 = clock();
+	ShellSort(arr);
+	clock_t end2 = clock();
+	cout << setw(25) << "Shell sort (random): " << setw(10) << (double)(end2 - start2) / CLOCKS_PER_SEC << endl;
+
+	FillUp(arr);
+	start2 = clock();
+	ShellSort(arr);
+	end2 = clock();
+	cout << setw(25) << "Shell sort (Up): " << setw(10) << (double)(end2 - start2) / CLOCKS_PER_SEC << endl;
+
+	FillDown(arr);
+	start2 = clock();
+	ShellSort(arr);
+	end2 = clock();
+	cout << setw(25) << "Shell sort (Down): " << setw(10) << (double)(end2 - start2) / CLOCKS_PER_SEC << endl << endl;
+
+
+
+	FillRand(arr);
+	clock_t start3 = clock();
+	SelectionSort(arr);
+	clock_t end3 = clock();
+	//show(arr);
+	cout << setw(25) << "Selection sort (random): " << setw(10) << (double)(end3 - start3) / CLOCKS_PER_SEC << endl;
+
+	FillUp(arr);
+	start3 = clock();
+	SelectionSort(arr);
+	end3 = clock();
+	cout << setw(25) << "Selection sort (Up): " << setw(10) << (double)(end3 - start3) / CLOCKS_PER_SEC << endl;
+
+	FillDown(arr);
+	start3 = clock();
+	SelectionSort(arr);
+	end3 = clock();
+	cout << setw(25) << "Selection sort (Down): " << setw(10) << (double)(end3 - start3) / CLOCKS_PER_SEC << endl;
+
+	delete[] arr;
+	return 0;
+}
 
 void FillRand(int* arr) {
 	srand(time(0));
@@ -23,25 +105,29 @@ void show(int* arr) {
 		cout << arr[i] << " ";
 	cout << endl;
 }
-void swap(int &a, int &b) {
+void swap(int& a, int& b) {
 	int temp = a;
 	a = b;
 	b = temp;
 }
 void InsertionSort(int* arr) {
 	int j;
+	long long k = 0;
 	for (int i = 0; i < N - 1; i++)
 	{
 		j = i + 1;
 		while (arr[j] < arr[j - 1])
 		{
 			swap(arr[j], arr[j - 1]);
-			j--;
+ 			j--;
+			k++;
 		}
 	}
+	cout << k << endl;
 }
-void ShellSort(int * arr)
+void ShellSort(int* arr)
 {
+	long long k = 0;
 	int d = N / 2, j;
 	while (d > 0)
 	{
@@ -52,20 +138,36 @@ void ShellSort(int * arr)
 			{
 				swap(arr[j], arr[j + d]);
 				j--;
+				k++;
 			}
 		}
 		d /= 2;
 	}
+	cout << k << endl;
 }
 void SelectionSort(int* arr) {
-	int j;
-	for (int i = 0; i < N; i++)
+	int min_index;
+	long long k = 0;
+	for (int i = 0; i < N - 1; i++)
 	{
-		j = i;
-		while (j < N - 1 && arr[j] > arr[j + 1])
-			j++;
-		swap(arr[j], arr[i]);
+		min_index = i;
+		for (int j = i + 1; j < N; j++)
+		{
+			if (arr[j] < arr[min_index])
+			{
+				min_index = j;
+				k++;
+			}
+			//k++;
+		}
+		if (min_index != i)
+		{
+			swap(arr[i], arr[min_index]);
+			k++;
+		}
+		k++;
 	}
+	cout << k << endl;
 }
 int increment(long  inc[], long  size) {
 	int p1, p2, p3, s;
@@ -99,73 +201,4 @@ void ShellInc(int* arr) {
 			arr[j + inc] = temp;
 		}
 	}
-}
-
-int main()
-{
-	int* arr = new int[N];
-
-	cout.setf(ios::fixed);
-	cout.precision(6);
-
-	FillRand(arr);
-	clock_t start1 = clock();
-	InsertionSort(arr);
-	clock_t end1 = clock();
-	cout << (double)(end1 - start1) / CLOCKS_PER_SEC << endl;
-
-	FillUp(arr);
-	start1 = clock();
-	InsertionSort(arr);
-	end1 = clock();
-	cout << (double)(end1 - start1) / CLOCKS_PER_SEC << endl;
-
-	FillDown(arr);
-	start1 = clock();
-	InsertionSort(arr);
-	end1 = clock();
-	cout << (double)(end1 - start1) / CLOCKS_PER_SEC << endl << endl;
-
-
-
-	FillRand(arr);
-	clock_t start2 = clock();
-	ShellSort(arr);
-	clock_t end2 = clock();
-	cout << (double)(end2 - start2) / CLOCKS_PER_SEC << endl;
-
-	FillUp(arr);
-	start2 = clock();
-	ShellSort(arr);
-	end2 = clock();
-	cout << (double)(end2 - start2) / CLOCKS_PER_SEC << endl;
-
-	FillDown(arr);
-	start2 = clock();
-	ShellSort(arr);
-	end2 = clock();
-	cout << (double)(end2 - start2) / CLOCKS_PER_SEC << endl << endl;
-
-
-
-	FillRand(arr);
-	clock_t start3 = clock();
-	SelectionSort(arr);
-	clock_t end3 = clock();
-	cout << (double)(end3 - start3) / CLOCKS_PER_SEC << endl;
-
-	FillUp(arr);
-	start3 = clock();
-	SelectionSort(arr);
-	end3 = clock();
-	cout << (double)(end3 - start3) / CLOCKS_PER_SEC << endl;
-
-	FillDown(arr);
-	start3 = clock();
-	SelectionSort(arr);
-	end3 = clock();
-	cout << (double)(end3 - start3) / CLOCKS_PER_SEC << endl;
-
-	delete[] arr;
-	return 0;
 }
